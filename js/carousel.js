56965b1008937362c6e4bb0569a66963950f45a9
huhu213 <15501003885@163.com>
2015-08-11 09:26:14 +0800

    first version

---
+++ js/carousel.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$(".editPicture").click(function(){
+		$("#fileupload").click();
+	})
+
+	$("#editPageEdit").click(function(){
+		$("#fileupload").click();
+	})
+	
+	$(".editLogo").click(function(){
+		$("#fileupload").click();
+	})
+	
+	$(".editPartner").click(function(){
+		$("#fileupload").click();
+	})
+		
+	$(".editStory").click(function(){
+		var $editPage = $("#editPage");
+		var bw = $("body").width();
+		var pw = $editPage.width();
+		$editPage.css("left",function(){
+			return 0.5 * (bw - pw);
+		})
+		$editPage.css("display","block");
+		$("#bg").css("display","block");
+		
+		$("#btnSave").click(function(){
+			$editPage.css("display", "none");
+			$("#bg").css("display", "none");
+		})
+		
+		$("#btnCancel").click(function(){
+			$editPage.css("display", "none");
+			$("#bg").css("display", "none");
+		})
+	})
+})
